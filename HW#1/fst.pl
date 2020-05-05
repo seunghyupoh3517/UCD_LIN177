7de9d6ac01a08b5a8103dfd6d75c647b771bb2be
Seunghyup Alex Oh <seunghyupoh3517@gmail.com>
2020-05-05 02:57:09 -0700

    Add files via upload

---
+++ HW#1/fst.pl
@@ -0,0 +1,36 @@
+:- use_module(library(tabling)).
+:- table go/3.
+:- set_prolog_flag(answer_write_options,[max_depth(0)]).
+
+% This is what we use to run the transducer
+fst(Input, Output) :-
+  initial(State),
+  go(State, Input, Output).
+
+% This is how we know that we've reached the end of the run
+go(CurrentState, [], []) :-
+  final(CurrentState).
+
+% Use a transition with no eps on either side
+go(CurrentState, [A|InString], [B|OutString]) :-
+  transition(CurrentState, A, NextState, B),
+  A \= eps,
+  B \= eps,
+  go(NextState, InString, OutString).
+
+% use a transition with eps on the input side
+go(CurrentState, InString, [B|OutString]) :-
+  transition(CurrentState, eps, NextState, B),
+  B \= eps,
+  go(NextState, InString, OutString).
+
+% use a transition with eps on the output side
+go(CurrentState, [A|InString], OutString) :-
+  transition(CurrentState, A, NextState, eps),
+  A \= eps,
+  go(NextState, InString, OutString).
+
+% use a transition with eps on both sides
+go(CurrentState, InString, OutString) :-
+  transition(CurrentState, eps, NextState, eps),
+  go(NextState, InString, OutString).
